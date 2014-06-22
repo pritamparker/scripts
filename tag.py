39a5c0c43c69c44d0a069dcd3649c2a7e3a0ef29
Ayush Shanker <ayushshanker@outlook.com>
2014-06-22 12:45:54 +0530

    did many things

--- tag.py
+++ tag.py
@@ -4,6 +4,8 @@
 """
 This is a quick hack script to tag songs downloaded from
 soundcloud (or elsewhere) using spotify/itunes's api
+
+note : works only on a xterm compatible terminal
 """
 
 __author__ = 'Ayush Shanker'
@@ -16,25 +18,9 @@ try:
     import requests
     from mutagenx.mp3 import MP3
     from mutagenx.mp4 import MP4, MP4Cover
-    from mutagenx.id3 import TIT2, TALB, TPE1, WOAF, TCOM, TCON, TDRC, APIC
+    from mutagenx.id3 import TIT2, TALB, TPE1, WOAF, TCOM, TCON, APIC, TRCK
 except ImportError:
-    print("dependencies not installed")
-
-
-
-def chooseTag(data):
-    for i in range(data['resultCount']):
-        track = data['results'][i]
-        print(color.BOLD + '\033[4m' + color.YELLOW + str(i), end=" : ")
-        print(color.RED + track['trackName'])
-        try:
-            print(color.BLUE + track['collectionName'], end=" - ")
-        except KeyError:
-            print("no album art :'(")
-        print(color.PURPLE + track['artistName'] + color.END)
-    print("enter your choice(0/1/2/3/4)")
-    choice = int(input())
-    return data['results'][choice]
+    print('dependencies not installed')
 
 
 class color:
@@ -107,7 +93,7 @@ def normalise(keywords):
     keywords=keywords.replace('(', '')
     keywords=keywords.replace(')', '')
     keywords=keywords.replace('**', '')
-    print("accept " + color.GREEN + keywords + color.END + "(y/n)")
+    print('accept ' + color.GREEN + keywords + color.END + '(y/n)')
     choice = input()
     if choice.lower() == 'e':
         exit()                      # a quick exit in case the file is tagged already
@@ -119,25 +105,25 @@ def normalise(keywords):
     return keywords
 
 def searchItunes(args, query):
-    url = "https://itunes.apple.com/search?term=%s&entity=musicTrack&limit=5" % query.replace(' ','+')
+    url = 'https://itunes.apple.com/search?term=%s&entity=musicTrack&limit=5' % query.replace(' ','+')
     response = requests.get(url, verify=False)
     j = json.loads(r.text)
     for i in range(j['resultCount']):
         track = j['results'][i]
-        print(color.YELLOW+str(i), end=" : ")
+        print(color.YELLOW+str(i), end=' : ')
         print(color.RED+ track['name'])
-        print(color.BLUE + track['album']['name'], end=" - ")
-        artists = ""
+        print(color.BLUE + track['album']['name'], end=' - ')
+        artists = ''
         for i in track['artists']:
-            artists = artists + i['name']+ ", "
+            artists = artists + i['name']+ ', '
         print(color.PURPLE + artists + color.END)
-    print("choose (0/1/2/3/4) ")
-    choice = str(input())
+    print('choose (0/1/2/3/4) ')
+    choice = int(input())
     trackSelected = j['results'][choice]
     tag = {'api':'itunes'}
     tag['title'] = trackSelected['trackName']
     tag['artist'] = trackSelected['artistName']
-    tag['album'] = trackSelected['collectionName']
+    tag['album'] = trackSelected['collectionName'] or 'Single'                  #itunes sometimes doesn't return album
     tag['album'] = trackSelected['primaryGenreName']
     tag['trackNumber'] = trackSelected['trackNumber']
     tag['totalTracks'] = trackSelected['trackCount']
@@ -145,25 +131,25 @@ def searchItunes(args, query):
         tag['artworkURL'] = trackSelected['artworkUrl100'].replace('100x100', '600x600')
     else:
         tag['artworkURL'] = trackSelected['artworkUrl100'].replace('100x100', '400x400')
-    tag['uri'] = trackSelected['collectionId'] + ":" + trackSelected['trackId']
+    tag['uri'] = trackSelected['collectionId'] + ':tagdata' + trackSelected['trackId']
     return tag
 
 def searchSpotify(args, query):
-    url = "https://api.spotify.com/v1/search?q=%s&type=track&limit=5" % query.replace(' ','%20')
+    url = 'https://api.spotify.com/v1/search?q=%s&type=track&limit=5' % query.replace(' ','%20')
     response = requests.get(url, verify=False)
     j = json.loads(r.text)['tracks']
     iterLimit = min(j['total']-j['offset']+1, j['limit'])
     for i in range(iterLimit):
         track = j['items'][i]
-        print(color.YELLOW+str(i), end=" : ")
+        print(color.YELLOW+str(i), end=' : ')
         print(color.RED+ track['name'])
-        print(color.BLUE + track['album']['name'], end=" - ")
-        artists = ""
+        print(color.BLUE + track['album']['name'], end=' - ')
+        artists = ''
         for i in track['artists']:
-            artists = artists + i['name']+ ", "
+            artists = artists + i['name']+ ', '
         print(color.PURPLE + artists + color.END)
-    print("choose (0/1/2/3/4) ")
-    choice = str(input())
+    print('choose (0/1/2/3/4) ')
+    choice = int(input())
     trackSelected = j['items'][choice]
     tag = {'api':'spotify'}
     tag['title'] = trackSelected['name']
@@ -184,71 +170,66 @@ def determineFormat(audio, track):
     :returns: mutagen audio object, search query text
     """
     name = track.lower()
-
-    if name.endswith("mp3"):
+    if name.endswith('mp3'):
         audio = MP3(os.path.abspath(track))
         try:
             print(str(audio['TPE1']))
             track = str(audio['TIT2'])
         except Exception:
             track = str(track)[0:-4]
-
-    if name.endswith("mp4") or name.endswith("m4a") or name.endswith("m4v"):
+    if name.endswith('mp4') or name.endswith('m4a') or name.endswith('m4v'):
         audio = MP4(os.path.abspath(track))
         try:
             track = str(audio[b'\xa9nam'][0])
         except Exception:
             track = str(track)[0:-4]
-
     return audio, track
 
 
 def main(argv):
     parser = argparse.ArgumentParser(description='Tag your mp3/m4a files using Spotify/Itunes metadata API')
-    parser.add_argument("-i","--itunes", help="retag using itunes",action="store_true")
-    parser.add_argument('songs', metavar='audiofile', nargs='+', help="audio files")
+    parser.add_argument('-i','--itunes', help='retag using itunes',action='store_true')
+    parser.add_argument('-b','--hi-res', help='use higher resolution cover arts',action='store_true')
+    parser.add_argument('songs', metavar='audiofile', nargs='+', help='audio files')
     args = parser.parse_args()
     for track in args.songs:
         audio, keywords = determineFormat(track)
         keywords = ' '.join(keywords.split())
         keywords = normalise(keywords)
         if args.i:
-            data = searchItunes(keywords)
+            data = searchItunes(args, keywords)
         else:
-            data = searchSpotify(keywords)
+            data = searchSpotify(args, keywords)
         try:
             tagmp3(data, audio)
-            print(color.GREEN + "tagged!  ⋆ ᴗ ⋆" + color.END)
+            print(color.GREEN + 'tagged!  ⋆ ᴗ ⋆' + color.END)
         except Exception:
-            print(color.RED + "failed!  (；ᴗ；)" + color.END)
+            print(color.RED + 'failed!  (；ᴗ；)' + color.END)
 
 
 def tagmp3(tagdata, audio):
-    audio["TIT2"] = TIT2(encoding=3, text=tagdata['title'])
-    audio["TPE1"] = TPE1(encoding=3, text=tagdata['artist'])
+    audio['TIT2'] = TIT2(encoding=3, text=tagdata['title'])
+    audio['TPE1'] = TPE1(encoding=3, text=tagdata['artist'])
+    audio['TALB'] = TALB(encoding=3, text=tagdata['album'])
     try:
-        audio["TALB"] = TALB(encoding=3, text=tagdata['album'])
-    except KeyError:
-        print("no album info provided :'(")                         # itunes sometimes doesn't return album field
-    try:
-        audio["TDRC"] = TDRC(encoding=3, text=tagdata['trackNumber'])
+        audio['TRCK'] = TRCK(encoding=3, text=tagdata['trackNumber'])
     except Exception:
         pass
+    if 'TDRC' in tagdata.keys():
+        audio['TDRC'] = TDRC(encoding=3, text=tagdata['releaseDate'][:10])
     #if 'TCON' not in audio.keys():
-    #    audio["TCON"] = TCON(encoding=3, text=tagdata['genre'])
+    #    audio['TCON'] = TCON(encoding=3, text=tagdata['genre'])
     if 'APIC:' not in audio.keys():
-        audio["APIC"]                                                           # to do
-    audio["WOAF"] = WOAF(encoding=3, text=tagdata['api']+tagdata['uri'])
+        imgdata=requests.get(tagdata['artwork']).content
+        audio['APIC'] = APIC(encoding=3,'image/jpeg',desc='Cover',data=imgdata)
+    audio['WOAF'] = WOAF(encoding=3, text=tagdata['api']+tagdata['uri'])
     audio.save()
 
 
 def tagmp4(tagdata, audio):
     audio[b'aART'] = tagdata['artist']
     audio[b'\xa9nam'] = tagdata['title']
-    try:
-        audio[b'\xa9alb'] = tagdata['album']
-    except KeyError:
-        print("no album art :'(")
+    audio[b'\xa9alb'] = tagdata['album']
     audio[b'\xa9ART'] = tagdata['artist']
     #audio[b'\xa9gen'] = tagdata['genre']
     audio[b'trkn'] = [tagdata['trackNumber'], tagdata['totalTracks']]
